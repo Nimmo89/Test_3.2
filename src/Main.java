95f6708f577f2d82aa50dc6af473912cb6bc35c2
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-07-18 18:22:18 +0700

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        int summa = 1100;
+        int balance = 100;
+        int bonus;
+        if (summa > 1000) {
+            bonus = summa / 100;
+        } else {
+            bonus = 0;
+        }
+        int amount = balance + summa + bonus;
+        System.out.println(amount);
+    }
 }
